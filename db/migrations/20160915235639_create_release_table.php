b7a28421cc09f89667e2810c6692680458f869b3
georgi <georgizk@gmail.com>
2016-09-15 20:23:25 -0400

    phinx migration, release model

---
+++ db/migrations/20160915235639_create_release_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateReleaseTable extends AbstractMigration
+{
+  public function up()
+  {
+    $this->execute("CREATE TABLE `release` (
+      `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+      `name` VARCHAR(128) NOT NULL,
+      `filename` VARCHAR(128) NOT NULL,
+      `description` TEXT NOT NULL,
+      `status` INT(10) UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `name` (`name`),
+    UNIQUE KEY `filename` (`filename`),
+    INDEX `status` (`status`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4");
+  }
+
+  public function down()
+  {
+    $this->execute("DROP TABLE `release`");
+  }
+}
---
+++ src/Imangascans/Model/Project.php
@@ -0,0 +1,13 @@
+<?php
+namespace Imangascans\Model;
+
+class Project
+{
+  const TABLE = 'project';
+
+  public $id;
+  public $name;
+  public $filename;
+  public $description;
+  public $status;
+}
