b9824fbbee234e374e8acbbdf518da3f4735ac6b
georgi <georgi@animebytes.tv>
2016-09-16 18:10:54 -0400

    rename class

--- db/migrations/20160916235641_create_release_archive_table.php
+++ db/migrations/20160916235641_create_release_archive_table.php
@@ -2,7 +2,7 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class CreateReleasePageTable extends AbstractMigration
+class CreateReleaseArchiveTable extends AbstractMigration
 {
 
   public function up()
