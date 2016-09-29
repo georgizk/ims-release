3d03bf33cc3b4a48187c3cc334b1427de2824aa5
Zack Mullaly <zsck@riseup.net>
2016-09-28 23:21:54 -0400

    Set up the HTTP server in main

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"./models"
+	"./endpoints"
+
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	p := models.NewProject("Project Test", "ptest", "A test project")
-	fmt.Println("Created test project\n", p)
-	r := models.NewRelease(1, "ch12")
-	fmt.Println("\nCreated test release\n", r)
-	page := models.NewPage(1, "manga/chapters/rnrl/001.jpg")
-	fmt.Println("\nCreated test page\n", page)
+	router := mux.NewRouter()
+	projectsRouter := router.PathPrefix("/projects").Subrouter()
+	endpoints.RegisterProjectHandlers(projectsRouter, nil, nil)
+
+	address := "0.0.0.0:3000"
+	fmt.Printf("Listening on %s\n", address)
+	http.ListenAndServe(address, router)
 }
